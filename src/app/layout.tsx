aa5d9fd3a0c9aeef2a4745c486967ce531bafea9
GulfamASB <gulfamsaifi0298@gmail.com>
2023-12-15 16:19:24 +0530

    it is the fourth commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,9 @@ import localFont from 'next/font/local'
 const myFont = localFont({ src: "./fonts/bodoni-mt-1.ttf" })
 
 export const metadata: Metadata = {
-  title: 'APlusStudio: Designing Dreams, Crafting Brilliance. ',
+  title: 'Crafting Dreams, Shaping Spaces: A Plus Studio Signature Interior Creations',
   description: 'APlusStudio is your gateway to a realm of visionary design and impeccable craftsmanship. With a passion for innovation and an unwavering commitment to excellence, we transform spaces into living works of art. Our diverse portfolio spans from contemporary concepts to timeless classics, reflecting our dedication to meeting and exceeding the unique aspirations of our clients. Explore the extraordinary with APlusStudio – where every project is a testament to the seamless fusion of creativity and precision.',
-  manifest:'manifest.webmanifest'
+  manifest:'manifest.webmanifest',
 }
 
 export default function RootLayout({
