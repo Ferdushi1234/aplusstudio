1ef2760800e4afdbf40d6902844e0ba2c68dabbf
GulfamASB <gulfamsaifi0298@gmail.com>
2023-12-15 16:31:02 +0530

    fpkm;lfdF

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import localFont from 'next/font/local'
 const myFont = localFont({ src: "./fonts/bodoni-mt-1.ttf" })
 
 export const metadata: Metadata = {
-  title: 'Crafting Dreams, Shaping Spaces: A Plus Studio Signature Interior Creations',
+  title: 'A+ Studio: Design Excellence',
   description: 'APlusStudio is your gateway to a realm of visionary design and impeccable craftsmanship. With a passion for innovation and an unwavering commitment to excellence, we transform spaces into living works of art. Our diverse portfolio spans from contemporary concepts to timeless classics, reflecting our dedication to meeting and exceeding the unique aspirations of our clients. Explore the extraordinary with APlusStudio – where every project is a testament to the seamless fusion of creativity and precision.',
   manifest:'manifest.webmanifest',
 }
