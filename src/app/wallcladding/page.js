bff185604add2c08ec86122adda937e1a1cf8d72
GulfamASB <gulfamsaifi0298@gmail.com>
2023-12-19 14:02:15 +0530

    it is the commit and commit

--- src/app/wallcladding/page.js
+++ src/app/wallcladding/page.js
@@ -15,7 +15,7 @@ const Wallcladding = () => {
                     <div className="video-docker absolute top-0 left-0 w-full h-96 overflow-hidden">
                         <video  className="min-w-full h-full absolute object-cover" src="/pro.mp4" type="video/mp4" autoPlay="{true}" muted loop></video>
                         <div style={{ marginTop: '4rem' }} className="py-10  relative text-white video-content space-y-2">
-                            <h2 className="font-light text-6xl">Whate is your Better choice</h2>
+                            <h1 className="font-light text-6xl">Whate is your Better choice</h1>
                             <h3 className="font-light text-3xl">with A Plus Studio</h3>
                             <Link  aria-label='ORDER' href='/products'>
                             <button type="button" className="shadow-lg shadow-cyan-500/50 ... text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">ORDER</button>
