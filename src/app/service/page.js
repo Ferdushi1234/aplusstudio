98a673dc25901a31594f8bf1718c56cf81714f4b
GulfamASB <gulfamsaifi0298@gmail.com>
2023-12-19 14:38:11 +0530

    ERewwefwesdf

--- src/app/service/page.js
+++ src/app/service/page.js
@@ -17,7 +17,7 @@ const Services = () => {
 <main style={{marginBottom: '-9rem'}} className={heroStyles.main_section}>
 <div  className=" py-24 bg-stone-100 ">
         <div className="relative z-10 mb-8 md:py-16 w-11/12 lg:w-10/12 xl:w-1200 m-auto ">
-          <h2  className=" text-4xl mb-8 tracking-tight font-extrabold text-center text-gray-900 dark:text-black">About Services</h2>
+          <h1  className=" text-4xl mb-8 tracking-tight font-extrabold text-center text-gray-900 dark:text-black">About Services</h1>
           <div className="space-y-16">
             <div
               className=" text-xl space-y-8 md:space-y-0 text-center md:text-left md:space-x-16 md:justify-center md:flex md:items-center ">
