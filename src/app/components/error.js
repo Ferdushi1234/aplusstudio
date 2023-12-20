bb47b8b57d6fd5afe3bdb6187a78be703d869442
GulfamASB <gulfamsaifi0298@gmail.com>
2023-12-20 16:37:43 +0530

    cvcv

--- src/app/components/error.js
+++ src/app/components/error.js
@@ -188,7 +188,7 @@ const NotFound = () => {
             <h1 className='text-sm font-bold md:text-base text-yellow-300 p-2 mb-4'>Sorry from A Plus Studio</h1>
             <p className="text-sm md:text-base text-yellow-300 p-2 mb-4">The stuff you were looking for doesn't exist
             </p>
-            <Link aria-label="Retry" href="/"
+            <Link aria-label="Retry" href="https://aplusstudio.in"
                 className="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-white rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent">
                 Retry</Link>
         </div>
