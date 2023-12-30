9c89b1225675908f9d7dd04d6f7b89ac9e48f300
GulfamASB <gulfamsaifi0298@gmail.com>
2023-12-30 11:57:53 +0530

    ASdasdasd

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,29 +5,8 @@ import Aboutus from '@/app/components/Aboutus'
 
 
 export const metadata = {
-  title: 'About',
+  title: 'About Us',
   description: 'About Us of A Plus Studio | it provides the Door with good quality and it also provide the wall cladding. | Discover  unequaled  design mastery at A Plus Studio.',
-  
-  icons: {
-    icon: [{ url: '/icon.png' }, new URL('/icon.png', 'https://aplusstudio.in')],
-    shortcut: ['/shortcut-icon.png'],
-    apple: [
-      { url: '/apple-icon.png' },
-      { url: '/apple-icon-x3.png', sizes: '180x180', type: 'image/png' },
-    ],
-    other: [
-      {
-        rel: 'apple-touch-icon-precomposed',
-        url: '/apple-touch-icon-precomposed.png',
-      },
-    ],
-  },
-  verification: {
-    google: 'bdLbDU8AMC-KYqW8F2RbsdY_MsKb8TWpxKg9QwUPWsw',
-    other: {
-      me: ['my-email', 'my-link'],
-    },
-  },
   openGraph: {
     title:'A Plus Studio',
     description: ' a plus studio of about, Discover  unequaled  design mastery at A Plus Studio – where invention meets complication. Elevate your space with bespoke innards that  review luxury and reflect your unique style. aplusstudio noida',
