62964285095c791f1c418dbe9a992b9d56efd946
GulfamASB <gulfamsaifi0298@gmail.com>
2024-01-10 13:25:41 +0530

    efesfd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,13 @@ export const metadata: Metadata = {
   manifest:'manifest.webmanifest',
   title: 'Home | A Plus Studio',
   description: 'A Plus Studio | it provides the Door with good quality and it also provide the wall cladding. | Discover  unequaled  design mastery at A Plus Studio. aplusstudio noida',
-  
+  generator: 'a plus studio',
+  applicationName: 'a plus studio',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['a plus studio', 'A plus studio', 'doors','doores', 'wallcladding', 'wall cladding','aplusstudio', 'aplusstudio noida', 'a plus studio noida','aplusstudio sector83','aplusstudio noida sector83','doors interior doors','door hinges','door design','concealed hinge','swinging door hinges','door','door swinging','aplusstudio india','wall cladding design','interior wall cladding','interior','pvc wall cladding','metal wall cladding','door mat','wooden door'],
+  authors: [{ name: 'a plus studio', url:'https://aplusstudio.in' }, { name: 'gulfam saifi', url: 'https://gulfam.in' }],
+  creator: 'gulfam saifi',
+  publisher: 'gulfam saifi',
 }
 
 
